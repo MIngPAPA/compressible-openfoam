9d94d293ad5b0b1f49121c44ec5fe3dc074de750
Pavanakumar Mohanamuraly <m.pavanakumar@gmail.com>
2012-11-05 20:55:48 +0530

    Implemented fully non-blocking communication and residue now being printed

--- eulerSolver.cpp
+++ eulerSolver.cpp
@@ -32,15 +32,17 @@ int main(int argc, char *argv[])
 
    /// Freestream values
    scalar rho_inf = 1.228 , p_inf = 101325.0, M_inf = 2.0 , CFL = 0.5;
-   scalar magU_inf = std::sqrt( 1.40e0 * p_inf / rho_inf ) * M_inf;
+   scalar magU_inf = std::sqrt( 1.40e0 * p_inf / rho_inf ) * M_inf , rhoResidMax = 0.0;
    vector u_inf ( magU_inf , 0.0 , 0.0 );
    // Unit face normals
    surfaceVectorField nf = mesh.Sf() / mesh.magSf();
-   label iter = 1;
-
+   long int iter = 1;
+       
    /// Time step loop
    while( runTime.loop() ) {
-     Info<< "Iteration = " << iter++ << nl << endl;
+
+     /// Post the non-blocking send/recv of fields
+     #include "initEvaluateFields.H"
 
      /// Construct the fluxes at faces
      #include "constructFaceFlux.H"
@@ -52,10 +54,10 @@ int main(int argc, char *argv[])
      #include "boundaryFlux.H"
      
      /// State update
-//     #include "stateUpdate.H"
      #include "stateUpdateLTS.H"
 
-     // Solution output
+     Info << "Iteration = " << iter++ << " Max residue = " << rhoResidMax << nl << endl;
+     /// Solution output
      runTime.write();
    }
  
